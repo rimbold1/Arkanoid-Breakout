a1f9b21df69961e5448cef4232faa2e3f2bca88c
volodymr <ivivzu2@gmail.com>
2024-11-18 16:21:17 +0200

    ad

--- js/Brick.js
+++ js/Brick.js
@@ -1,9 +1,10 @@
 import { Sprite } from "pixi.js";
 
 export class Brick extends Sprite {
-    constructor(textures) {
+    constructor(textures, typeID) {
     super(textures)
         this.anchor.set(0.5);
         this.num = Math.floor(Math.random()*16);
+        this.typeID = typeID;
     }
 };
